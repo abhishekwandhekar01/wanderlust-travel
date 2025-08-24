5f3f7f2683a31f34d226aaa948892922c9e32c8b
abhishekwandhekar <abhishekwandhekar4@gmail.com>
2025-08-24 11:58:36 +0530

    Add Project Files

---
+++ utils/ExpressError.js
@@ -0,0 +1,10 @@
+class ExpressError extends Error {
+    constructor(statusCode = 500, message = "Something went wrong!") {
+        super();
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
+
+
+module.exports = ExpressError;
