86a414a733620d8e612d4d1793e06cb3dd0c0cac
abhishekwandhekar <abhishekwandhekar4@gmail.com>
2025-11-19 00:32:02 +0530

    fixed login issue

--- app.js
+++ app.js
@@ -72,6 +72,11 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
+// make currUser available in all views immediately after passport.session()
+app.use((req, res, next) => {
+    res.locals.currUser = req.user || null;
+    next();
+});
 passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
